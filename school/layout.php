9dd82f81ce2bf2778df89170a3c0493142a01842
durgalakshmiv <durgalakshmiv95@gmail.com>
2022-01-04 14:35:43 -0600

    Added layout page

--- school/layout.php
+++ school/layout.php
@@ -11,8 +11,9 @@ if(empty($userid)){
 }
 $pagetitle = 'Schools';
 $subtitle = 'Schools List';
-$result = [];
 $toggleaddbutton = 1;
+$result = [];
+//$result = ['status' => '1', 'message' => 'Added Successfully'];
 
 ?>
 <head>
@@ -29,58 +30,54 @@ $toggleaddbutton = 1;
 <body>
     <div id="wrapper">
         <?php require_once('../layout/sidebar.php'); ?>
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+            <?php require_once('../layout/header.php'); 
+            require_once('../layout/breadcrumb.php');
+            ?>
+            <div class="wrapper wrapper-content ">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox ">
+                            <div class="ibox-content">
+                                <?php if(!empty($result)){
+                                    $notification_class = ($result['status'] == '1')  ? 'success' : 'danger'; 
+                                    ?> 
+                                    <div class="alert alert-<?= $notification_class; ?> alert-dismissable" id="success-alert">
+                                        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                        <?= $result['message']; ?>. 
+                                    </div>        
+                                <?php  } ?>
+                            
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover dataTables-example" >
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" class="sort" data-sort="name">ID</th>
+                                                <th scope="col" class="sort" data-sort="name">School Name</th>
+                                                <th scope="col" class="sort" data-sort="budget">Address</th>
+                                                <th scope="col" class="sort" data-sort="status">Email</th>
+                                                <th scope="col" class="sort" data-sort="completion">Mobile Number</th>
+                                                <th scope="col" class="sort" data-sort="completion">Registration Number</th>
+                                                <th scope="col" class="sort" data-sort="completion">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
 
-    <div id="page-wrapper" class="gray-bg dashbard-1">
-        <?php require_once('../layout/header.php'); 
-        require_once('../layout/breadcrumb.php');
-        ?>
-        <div class="wrapper wrapper-content ">
-            <div class="row">
-                <div class="col-lg-12">
-                <div class="ibox ">
-                    <div class="ibox-content">
-                        <?php if(!empty($result)){
-                            $notification_class = ($result['status'] == '1')  ? 'success' : 'danger'; 
-                            ?> 
-                            <div class="alert alert-<?= $notification_class; ?> alert-dismissable" id="success-alert">
-                                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                                <?= $result['message']; ?>. 
-                    </div>        
-                            <?php  } ?>
-                    
-                    <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover dataTables-example" >
-                    <thead>
-                    <tr>
-                    <th scope="col" class="sort" data-sort="name">ID</th>
-                    <th scope="col" class="sort" data-sort="name">School Name</th>
-                    <th scope="col" class="sort" data-sort="budget">Address</th>
-                    <th scope="col" class="sort" data-sort="status">Email</th>
-                    <th scope="col" class="sort" data-sort="completion">Mobile Number</th>
-                    <th scope="col" class="sort" data-sort="completion">Registration Number</th>
-                    <th scope="col" class="sort" data-sort="completion">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-
-                    </table>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
                 </div>
-            </div>
-            </div>
-        </div>          
+            </div>          
 
-              
-        <?php require_once('../layout/footer.php');?>
-    </div>
-    <!-- page wrapper -->
-
+                
+            <?php require_once('../layout/footer.php');?>
+        </div>
+        <!-- page wrapper -->
     </div>
     <!-- wrapper -->
- <?php require_once('../layout/footerscripts.php'); ?>
+    <?php require_once('../layout/footerscripts.php'); ?>
 </body>
 </html>
