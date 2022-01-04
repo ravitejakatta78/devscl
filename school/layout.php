09ce2bd4636624474b9e4b5bde562c464a9d80cf
durgalakshmiv <durgalakshmiv95@gmail.com>
2022-01-04 14:20:41 -0600

    Added school folder

---
+++ school/layout.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<?php
+session_start();
+
+require_once('../common.php');
+
+$userid = current_userid(); 
+if(empty($userid)){
+	header("Location: ../login.php");
+}
+$pagetitle = 'Schools';
+$subtitle = 'Schools List';
+$result = [];
+$toggleaddbutton = 1;
+
+?>
+<head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>INSPINIA | Dashboard</title>
+        <?php require_once('../layout/headerscripts.php'); ?>
+
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php require_once('../layout/sidebar.php'); ?>
+
+    <div id="page-wrapper" class="gray-bg dashbard-1">
+        <?php require_once('../layout/header.php'); 
+        require_once('../layout/breadcrumb.php');
+        ?>
+        <div class="wrapper wrapper-content ">
+            <div class="row">
+                <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-content">
+                        <?php if(!empty($result)){
+                            $notification_class = ($result['status'] == '1')  ? 'success' : 'danger'; 
+                            ?> 
+                            <div class="alert alert-<?= $notification_class; ?> alert-dismissable" id="success-alert">
+                                <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                <?= $result['message']; ?>. 
+                    </div>        
+                            <?php  } ?>
+                    
+                    <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover dataTables-example" >
+                    <thead>
+                    <tr>
+                    <th scope="col" class="sort" data-sort="name">ID</th>
+                    <th scope="col" class="sort" data-sort="name">School Name</th>
+                    <th scope="col" class="sort" data-sort="budget">Address</th>
+                    <th scope="col" class="sort" data-sort="status">Email</th>
+                    <th scope="col" class="sort" data-sort="completion">Mobile Number</th>
+                    <th scope="col" class="sort" data-sort="completion">Registration Number</th>
+                    <th scope="col" class="sort" data-sort="completion">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+
+                    </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>          
+
+              
+        <?php require_once('../layout/footer.php');?>
+    </div>
+    <!-- page wrapper -->
+
+    </div>
+    <!-- wrapper -->
+ <?php require_once('../layout/footerscripts.php'); ?>
+</body>
+</html>
